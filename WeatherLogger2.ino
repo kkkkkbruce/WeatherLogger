8eaf3edf42bd567129c729a43b611ffbd0a4ca53
Kevin Bruce <brucekev@gmail.com>
2016-06-08 07:08:06 -0500

    Removed Pressure smoothing, unifide error reporting

--- WeatherLogger2.ino
+++ WeatherLogger2.ino
@@ -1,34 +1,33 @@
 /*
- #WeatherLogger
- ----
- Hardware: 
- *LCD screen 20x4 using Adafruit i2c/SPI LCD backpack using MCP23008 I2C expander
- (http://www.ladyada.net/products/i2cspilcdbackpack/index.html)
- *DHT 22 Temperature and Humidity sensor
- *MPL115A2 Pressure sensor
- *Adafruit Data Logger Shield
- 
- This sketch shows Date & Time & current sensor readings to LCD
- And logs sensor readings to CVC file on SD card every 5 secconds
- 
-  The circuit:
-  The LCD connected via i2c
- * 5V to Arduino 5V pin
- * GND to Arduino GND pin
- * CLK to Analog #5
- * DAT to Analog #4
- 
- The DHT 22
- * VCC to Arduino 5V pin
- * GND to Arduino GND pin
- * DAT to Digital #2
- * 10K resistor between VCC & DAT as pull up
- 
- The MPL115A2 via i2c
- * VCC to Arduino 5V pin
- * GND to Arduino GND pin
- * SCL to Analog #5
- * SDA to Analog #4
+#WeatherLogger
+
+This sketch shows Date & Time & current sensor readings to LCD and logs sensor readings to CVC file on SD card every 2 secconds
+
+Hardware:
+* LCD screen 20x4 using Adafruit i2c/SPI LCD backpack
+* DHT 22 Temperature and Humidity sensor
+* Adafruit MPL115A2 Pressure sensor breakout
+* Adafruit Data Logger Shield
+
+
+The circuit:
+The LCD connected via i2c
+* 5V to Arduino 5V pin
+* GND to Arduino GND pin
+* CLK to Analog #5
+* DAT to Analog #4
+
+The DHT 22
+* VCC to Arduino 5V pin
+* GND to Arduino GND pin
+* DAT to Digital #2
+* 10K resistor between VCC & DAT as pull up
+
+The MPL115A2 via i2c
+* VCC to Arduino 5V pin
+* GND to Arduino GND pin
+* SCL to Analog #5
+* SDA to Analog #4
 
 */
 
@@ -49,6 +48,7 @@
 // the last 10 reads if power is lost but it uses less power and is much faster!
 #define SYNC_INTERVAL 20000 // mills between calls to flush() - to write data to the card
 uint32_t syncTime = 0; // time of last sync()
+
 #define ECHO_TO_SERIAL   0 // echo data to serial port
 #define WAIT_TO_START    0 // Wait for serial input in setup()
 
@@ -63,9 +63,6 @@ RTC_DS1307 rtc;
 DHT dht(DHTPIN, DHTTYPE);
 //mp115a2 baramoter sensor
 Adafruit_MPL115A2 mpl115a2;
-// the digital pins that connect to the LEDs
-#define redLEDpin 3
-#define greenLEDpin 4
 
 // for the data logging shield, we use digital pin 10 for the SD cs line
 const int chipSelect = 10;
@@ -83,28 +80,24 @@ char monthsOfTheYear[12][4] ={"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "
 float h = 0.0;
 float f = 0.0;
 
-#define AvgSize  10
+//#define AvgSize  10
 float pressureKPA = 0;
-float bpHistory[AvgSize];
+//float bpHistory[AvgSize];
 
 void error(char *str)
 {
-  Serial.print("error: ");
+  lcd.clear();
+  lcd.print("error: ");
+  lcd.println(str);
+#if ECHO_TO_SERIAL
   Serial.println(str);
-  
-  // red LED indicates error
-  digitalWrite(redLEDpin, HIGH);
-
+#endif  //ECHO_TO_SERIAL
   while(1);
 }
 
 void setup() {
   Serial.begin(9600);
   Serial.println();
-  
-  // use debugging LEDs
-  pinMode(redLEDpin, OUTPUT);
-  pinMode(greenLEDpin, OUTPUT);
   
 #if WAIT_TO_START
   Serial.println("Type any character to start");
@@ -155,18 +148,14 @@ void setup() {
   
   //Start up the Clock
   if (! rtc.begin()) {
-    lcd.print("RTC failed");
-    logfile.println("RTC failed");
-#if ECHO_TO_SERIAL
-    Serial.println("RTC failed");
-#endif  //ECHO_TO_SERIAL
+    error("RTC failed");
     while (1);
   }
   //Uncomment the following line to set the date and time of the clock
   //to the compile time of the sketch
   //rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
   if (! rtc.isrunning()) {
-    Serial.print("RTC is NOT running!");
+    error("RTC is NOT running!");
     // following line sets the RTC to the date & time this sketch was compiled
     rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
     // This line sets the RTC with an explicit date & time, for example to set
@@ -178,16 +167,12 @@ void setup() {
 #if ECHO_TO_SERIAL
   Serial.println("millis,stamp,datetime,humidity,temp,pressure");
 #endif //ECHO_TO_SERIAL
-  
-  //populate Barometric Pressure History
-  int i;
-  for(i=0;i<AvgSize;i=i+1){
+/*  
+  //populate Barometric Pressure History Array
+  for(int i=0;i<AvgSize;i=i+1){
     bpHistory[i] = mpl115a2.getPressure();
-    /*Serial.print(i);
-    Serial.print(" ");
-    Serial.println(bpHistory[i],4);
-    */
   }
+*/
 }
 
 void loop() {
@@ -195,8 +180,6 @@ void loop() {
 
   // delay for the amount of time we want between readings
   delay((LOG_INTERVAL -1) - (millis() % LOG_INTERVAL));
-  
-  digitalWrite(greenLEDpin, HIGH);
   
   // log milliseconds since starting
   uint32_t m = millis();
@@ -242,6 +225,7 @@ void loop() {
   Serial.print(now.second(), DEC);
   Serial.print('"');
 #endif //ECHO_TO_SERIAL
+
   // Reading temperature or humidity takes about 250 milliseconds!
   // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
   h = dht.readHumidity();
@@ -250,25 +234,23 @@ void loop() {
 
   // Check if any reads failed and exit early (to try again).
   if (isnan(h) || isnan(f)) {
-    Serial.println("Failed to read from DHT sensor!");
+    error("Failed to read from DHT sensor!");
     return;
   }
+ /* 
   //Shift and append Barometric Pressure History
-  int i, sum;
-  sum = 0;
-  for(i=0;i<AvgSize-1;i=i+1){
+  int sum = 0;
+  for(int i=0;i<AvgSize-1;i=i+1){
     bpHistory[i] = bpHistory[i+1];
     sum = sum + bpHistory[i];
-    /*Serial.print(i);
-    Serial.print(" ");
-    Serial.println(bpHistory[i],4);
-    */
   }
   bpHistory[AvgSize-1] = mpl115a2.getPressure();
   sum = sum + bpHistory[AvgSize-1];
   pressureKPA = sum/AvgSize;  
+ */
+  pressureKPA = mpl115a2.getPressure();
   
-    logfile.print(", ");    
+  logfile.print(", ");    
   logfile.print(h);
   logfile.print(", ");    
   logfile.print(f);
@@ -288,28 +270,7 @@ void loop() {
   Serial.println();
 #endif // ECHO_TO_SERIAL
 
-  digitalWrite(greenLEDpin, LOW);
-  
-  UpdateLCD();
-
-  // Now we write data to disk! Don't sync too often - requires 2048 bytes of I/O to SD card
-  // which uses a bunch of power and takes time
-  if ((millis() - syncTime) < SYNC_INTERVAL) return;
-  syncTime = millis();
-  
-  // blink LED to show we are syncing data to the card & updating FAT!
-  digitalWrite(redLEDpin, HIGH);
-  logfile.flush();
-  digitalWrite(redLEDpin, LOW);
-}
-
-void UpdateLCD(){
-  //lcd.clear();
-  // set the cursor to column 0, line 1
-  // (note: line 1 is the second row, since counting begins with 0):
-
   lcd.setCursor(0, 1);
-  DateTime now = rtc.now();
   char DTStr [20];
   int n;
   n = sprintf(DTStr, "%s %s %02d  %02d:%02d:%02d", daysOfTheWeek[now.dayOfTheWeek()],monthsOfTheYear[now.month()],now.day(),now.hour(),now.minute(),now.second());
@@ -326,5 +287,10 @@ void UpdateLCD(){
   lcd.print("Press: "); 
   lcd.print(pressureKPA); 
   lcd.print(" kPa");
+  
+  // Now we write data to disk! Don't sync too often - requires 2048 bytes of I/O to SD card
+  // which uses a bunch of power and takes time
+  if ((millis() - syncTime) < SYNC_INTERVAL) return;
+  syncTime = millis();
+  logfile.flush();
 }
-
